621a1d7c4ab494bd9fba36f55aaeba7328e2de20
MistEO <mistereo@hotmail.com>
2021-11-05 22:52:48 +0800

    feat.支持除纯文本部分消息

--- handlers/functions/user/autoreply.py
+++ handlers/functions/user/autoreply.py
@@ -4,14 +4,17 @@ from handlers.constraint import FuncInterface
 from core import AmiyaBot
 import time
 import random
+import json
 
 reply_count_threshold = 3
 
 @FuncInterface.is_disable_func(function_id='autoreply')
 def autoreply(data: Message, bot: AmiyaBot):
+    chain = json.dumps(data.raw_chain)
+
     reply_list = ReplyRecord.select().where(
         ReplyRecord.group_id==data.group_id,
-        ReplyRecord.pre_msg==data.text_origin,
+        ReplyRecord.pre_msg==chain,
         ReplyRecord.count >= reply_count_threshold
     ).order_by(ReplyRecord.count.desc()).limit(3)
 
@@ -19,9 +22,11 @@ def autoreply(data: Message, bot: AmiyaBot):
         rand_index = random.randint(0, len(reply_list) - 1)
         reply_rec = reply_list[rand_index]
         with bot.send_custom_message(group_id=data.group_id) as reply:
-            reply.text(reply_rec.reply_msg)
+            reply.chain = json.loads(reply_rec.reply_msg)
 
 def record(data: Message):
+    chain = json.dumps(data.raw_chain)
+
     msg_list = MsgRecord.select().where(
         MsgRecord.group_id==data.group_id
         ).order_by(
@@ -29,14 +34,14 @@ def record(data: Message):
         ).limit(1)
     if msg_list:
         msg = msg_list[0]
-        if msg.msg == data.text_origin:
+        if msg.msg == data.message:
             pass    # 说明是在复读
         else:
-            print('latest message: %s' % msg.msg, 'cur message : %s' % data.text_origin)
+            print('latest message: %s' % msg.msg, 'cur message : %s' % chain)
             reply_list = ReplyRecord.select().where(
                 ReplyRecord.group_id==msg.group_id,
                 ReplyRecord.pre_msg==msg.msg,
-                ReplyRecord.reply_msg==data.text_origin
+                ReplyRecord.reply_msg==chain
             ).limit(1)
             if reply_list:
                 reply = reply_list[0]
@@ -48,13 +53,13 @@ def record(data: Message):
                 ReplyRecord.insert(
                     group_id=msg.group_id,
                     pre_msg=msg.msg,
-                    reply_msg=data.text_origin
+                    reply_msg=chain
                 ).execute()
 
     MsgRecord.insert(
         group_id=data.group_id, 
         user_id=data.user_id, 
-        msg=data.text_origin, 
+        msg=chain, 
         time=time.time()
     ).execute()
     
